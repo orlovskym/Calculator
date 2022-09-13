36b7da4a959b069b83ba3237006468d8139c260f
Mark Orlovsky <orlovskym@gmail.com>
2022-09-12 21:25:06 -0700

    add basic calculator function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,11 @@
+let a = '0'
+let b = ''
+let sign = ''
+let buttons = document.querySelectorAll("button");
+let screen = document.querySelector(`#screen`);
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let operators = ["+", "-", "*", "/"]
+
 function add(a, b) {
     return a + b;
 }
@@ -15,18 +23,60 @@ function divide(a, b) {
 }
 
 function operate(a, b, sign) {
+    a = parseInt(a);
+    b = parseInt(b);
     switch (sign) {
         case "+":
-            add(a, b);
+            return (add(a, b));
             break;
         case "-":
-            subtract(a, b);
+            return (subtract(a, b));
             break;
         case "*":
-            multiply(a, b);
+            return (multiply(a, b));
             break;
         case "/":
-            divide(a, b);
+            return (divide(a, b));
             break;
     }
 }
+
+
+for (i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', e => {
+        buttonPress(e.target.innerText);
+    })
+}
+
+
+function buttonPress(which) {
+    console.log(which);
+    if (which == "CLR") {
+        clearScreen();
+    } else if (operators.includes(which)) {
+        sign = which;
+    } else if (which == "=") {
+        a = operate(a, b, sign);
+        sign = '';
+        b = '';
+    } else {
+        if (!sign) {
+            if (a == 0) a = which;
+            else a += which;
+        }
+        if (sign) b += which;
+    }
+    updateScreen();
+}
+
+function updateScreen() {
+    screen.innerText = a + sign + b;
+}
+
+function clearScreen() {
+    a = '0';
+    b = '';
+    sign = '';
+}
+
+updateScreen();
